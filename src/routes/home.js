2174a3e00eb2f0c40b360ef9c1dd1c2dd329c1e7
TomasS <punkertje66@gmail.com>
2020-06-12 12:51:25 +0200

    testing for meesterproef

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,8 +3,37 @@ const router = express.Router();
 const path = require('path')
 
 
-router.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'../../static/index.html'))
+async function getUserPage(handle){
+    // const instagramJson = await axios.get(`https://www.instagram.com/${handle}/?__a=1`);
+    // const instagramJson = await (await fetch(`https://www.instagram.com/${handle}/?__a=1`)).json();
+    const instagramJson = await fetch(`https://www.instagram.com/${handle}/?__a=1`);
+    // const {
+    //      graphql:{
+    //          user:{
+    //             edge_owner_to_timeline_media: {
+    //                 edges
+    //             }
+    //          }
+    //      }
+    // } = await instagramJson.data;
+    console.log('handle from server ', handle )
+    // console.log(instagramJson.data)
+
+    const data = await instagramJson.json()
+    console.log('data', data)
+    console.log(instagramJson.json())
+
+    return data
+    // return getShortCodes(instagramJson.graphql.user.edge_owner_to_timeline_media.edges)
+}
+
+router.get('/', async (req, res) => {
+
+   const data = await getUserPage()
+    
+   res.send('wef')
+
+    // res.sendFile(path.resolve(__dirname,'../../static/index.html'))
 })
 
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config({path:__dirname+'../../.env'})
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 7070
+const axios = require('axios')
 
 const designShirt = require('./routes/design-shirt.js')
 const home = require('./routes/home.js')
@@ -21,14 +22,42 @@ app
     // .use(partials())
     .set('view-engine', 'ejs')
     .set('views', path.join(__dirname,'views'))
-
-    .use(express.static(path.join(__dirname, '../static')))
-    .use('/', designShirt)
-    .use('/', home)
-    .use('/', save)
-    .use('/', result)
+    .use(express.json())
+    // .use(express.static(path.join(__dirname, '../static')));
+    // .use('/', designShirt)
+    // .use('/', home)
+    // .use('/', save)
+    // .use('/', result)
     // .use('/', detail)
     // .use('/', search)
-
+    async function getUserPage(handle){
+        const instagramJson = await axios.get(`https://www.instagram.com/${handle}/?__a=1`);
+        // const instagramJson = await (await fetch(`https://www.instagram.com/${handle}/?__a=1`)).json();
+        // const instagramJson = await fetch(`https://www.instagram.com/${handle}/?__a=1`);
+        // const {
+        //      graphql:{
+        //          user:{
+        //             edge_owner_to_timeline_media: {
+        //                 edges
+        //             }
+        //          }
+        //      }
+        // } = await instagramJson.data;
+        console.log('handle from server ', handle )
+        // console.log(instagramJson.data)
     
-    .listen(port, () => console.log(`BT ${port}!`))
+        // const data = await instagramJson.json()
+        console.log('data', instagramJson.data)
+        // console.log(instagramJson.json())
+    
+        return instagramJson.data
+        // return getShortCodes(instagramJson.graphql.user.edge_owner_to_timeline_media.edges)
+    }
+    
+    app.get('/', async (req, res) => {
+    
+       const data = await getUserPage('eminem')
+       res.json(data)
+    })
+    
+    .listen(port, () => console.log(`B ${port}!`))
